cc97cd7761a5bdc7e605db566547d34652053e44
Marilene Soares da Costa <cmarilene@vmware.com>
2023-06-27 16:37:21 -0600

    add lesson 1

--- L1.py
+++ L1.py
@@ -16,5 +16,92 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 response = get_completion("what is the capital of France")
-
+print("What is the capital of France?")
 print(response)
+
+def get_completion_from_messages(messages,
+                                 model="gpt-3.5-turbo",
+                                 temperature=0,
+                                 max_tokens=500):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+        max_tokens=max_tokens, # the maximum number of tokens the model can ouptut
+    )
+    return response.choices[0].message["content"]
+
+messages =  [
+    {'role':'system',
+     'content':"""You are an assistant who\
+ responds in the style of Dr Seuss."""},
+    {'role':'user',
+     'content':"""write me a very short poem\
+ about a happy carrot"""},
+]
+response2 = get_completion_from_messages(messages, temperature=1)
+print("\nwrite a short poem about a happy carrot in Dr Seuss style...")
+print(response2)
+
+# length
+messages =  [
+    {'role':'system',
+     'content':'All your responses must be \
+one sentence long.'},
+    {'role':'user',
+     'content':'write me a story about a happy carrot'},
+]
+response3 = get_completion_from_messages(messages, temperature =1)
+print("\n one sentence long...")
+print(response3)
+
+# combined
+messages =  [
+    {'role':'system',
+     'content':"""You are an assistant who \
+responds in the style of Dr Seuss. \
+All your responses must be one sentence long."""},
+    {'role':'user',
+     'content':"""write me a story about a happy carrot"""},
+]
+response = get_completion_from_messages(messages,
+                                        temperature =1)
+print("\n combined...")
+print(response)
+
+def get_completion_and_token_count(messages,
+                                   model="gpt-3.5-turbo",
+                                   temperature=0,
+                                   max_tokens=500):
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+
+    content = response.choices[0].message["content"]
+
+    token_dict = {
+        'prompt_tokens':response['usage']['prompt_tokens'],
+        'completion_tokens':response['usage']['completion_tokens'],
+        'total_tokens':response['usage']['total_tokens'],
+    }
+
+    return content, token_dict
+
+messages = [
+    {'role':'system',
+     'content':"""You are an assistant who responds\
+ in the style of Dr Seuss."""},
+    {'role':'user',
+     'content':"""write me a very short poem \ 
+ about a happy carrot"""},
+]
+response, token_dict = get_completion_and_token_count(messages)
+
+print("\n completion and token count...")
+print(response)
+
+print(token_dict)
